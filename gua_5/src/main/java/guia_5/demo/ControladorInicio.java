c406c3e8657c50632863ec9a76dcbe340b853751
LuisRicardo77 <luissayas2018@gmail.com>
2024-04-07 12:21:38 -0500

    first commit

---
+++ gua_5/src/main/java/guia_5/demo/ControladorInicio.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia_5.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ *
+ * @author ldric
+ */
+
+
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    @Value("${index.nensaje}")
+    String dato;
+    @GetMapping("/")
+    public String inicio(Model modelo){
+        String mensaje = "saludos";
+        modelo.addAttribute("mensaje", mensaje);
+        modelo.addAttribute("dato", dato);
+        log.info("Ejecutando el controlador de inicio");
+        return "index";
+    }
+}
